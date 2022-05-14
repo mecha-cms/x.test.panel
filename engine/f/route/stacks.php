fc6b7740d2c06947c32cb94454255320b1b99a84
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-05-14 12:18:22 +0700

    Update

--- engine/f/route/stacks.php
+++ engine/f/route/stacks.php
@@ -65,6 +65,67 @@ function stacks($_) {
         ],
         'type' => 'stacks'
     ];
+    $lot['rows'] = [
+        'lot' => [],
+        'tags' => ['p' => true],
+        'type' => 'rows'
+    ];
+    $lot['rows']['lot'][0] = [
+        'lot' => [],
+        'type' => 'columns'
+    ];
+    $lot['rows']['lot'][0]['lot'][0] = [
+        'lot' => [
+            0 => [
+                'has' => ['gap' => true],
+                'lot' => [
+                    0 => [
+                        'content' => '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>',
+                        'icon' => 'M21 11H3V9H21V11M21 13H3V15H21V13Z',
+                        'stack' => 10,
+                        'title' => 'Widget 1'
+                    ],
+                ],
+                'type' => 'stacks'
+            ]
+        ],
+        'size' => '4/6',
+        'stack' => 10
+    ];
+    $lot['rows']['lot'][0]['lot'][1] = [
+        'lot' => [
+            0 => [
+                'current' => false, // Close all stack(s)
+                'has' => ['gap' => true],
+                'lot' => [
+                    0 => [
+                        'content' => '<p>Content goes here.</p>',
+                        'icon' => 'M21 11H3V9H21V11M21 13H3V15H21V13Z',
+                        'stack' => 10,
+                        'title' => 'Widget 1',
+                        'toggle' => true
+                    ],
+                    1 => [
+                        'content' => '<p>Content goes here.</p>',
+                        'icon' => 'M21 11H3V9H21V11M21 13H3V15H21V13Z',
+                        'stack' => 20,
+                        'title' => 'Widget 2',
+                        'toggle' => true
+                    ],
+                    2 => [
+                        'content' => '<p>Content goes here.</p>',
+                        'icon' => 'M21 11H3V9H21V11M21 13H3V15H21V13Z',
+                        'stack' => 30,
+                        'title' => 'Widget 3',
+                        'toggle' => true
+                    ]
+                ],
+                'type' => 'stacks'
+            ]
+        ],
+        'size' => '2/6',
+        'stack' => 20
+    ];
     $_['lot']['desk']['lot']['form']['lot'][1]['lot'] = $lot;
     return $_;
 }
