bbffba6b8fdd0908c003e01ba57475dad9105fef
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-12-30 21:28:38 +0700

    Update `value-after` and `value-before`
    With `gist` and `icon` keys.

--- engine/f/route/fields.php
+++ engine/f/route/fields.php
@@ -278,23 +278,23 @@ function fields($_) {
                 'type' => 'text'
             ],
             'text-with-before' => [
+                'gist' => 'http://',
                 'name' => 'default[text-with-before]',
                 'title' => 'Text',
-                'type' => 'text',
-                'value-before' => 'http://'
+                'type' => 'text'
             ],
             'text-with-after' => [
+                'gist' => ["", '.html'],
                 'name' => 'default[text-with-after]',
                 'title' => 'Text',
-                'type' => 'text',
-                'value-after' => '.html'
+                'type' => 'text'
             ],
             'text-with-before-after' => [
+                'gist' => ["", 'text'], // Text on the right
+                'icon' => ['M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z', ""], // Icon on the left
                 'name' => 'default[text-with-before-after]',
                 'title' => 'Text',
                 'type' => 'text',
-                'value-after' => 'text',
-                'value-before' => ['icon' => 'M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z']
             ],
             'text-with-lot' => [
                 'hint' => 'Select color&hellip;',
